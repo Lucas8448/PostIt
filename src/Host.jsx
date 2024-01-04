fbadace12991843999f78a4f253666f3cf29022c
Lucas Bateson <lucas.bateson@gmail.com>
2024-01-04 16:52:47 +0100

    Update to functionality and conditional rendering

--- src/Host.jsx
+++ src/Host.jsx
@@ -2,75 +2,72 @@ import React, { useState, useEffect } from 'react';
 import Peer from 'peerjs';
 
 const Host = () => {
-  const [peerId, setPeerId] = useState('');
-  const [peer, setPeer] = useState(null);
-  const [conn, setConn] = useState(null);
-  const [ideas, setIdeas] = useState([]);
-  const [users, setUsers] = useState([]);
-  const [message, setMessage] = useState(''); // Add this line
+ const [peerId, setPeerId] = useState('');
+ const [conn, setConn] = useState(null);
+ const [ideas, setIdeas] = useState([]);
+ const [message, setMessage] = useState('');
 
-  function generateID() {
-    let numbers = '';
-    for (let i = 0; i < 4; i++) {
-      numbers += Math.floor(Math.random() * 10);
-    }
-    return numbers;
-  }
+ function generateID() {
+   let numbers = '';
+   for (let i = 0; i < 4; i++) {
+     numbers += Math.floor(Math.random() * 10);
+   }
+   return numbers;
+ }
 
-  useEffect(() => {
-    const newPeer = new Peer(generateID());
-    newPeer.on('open', id => {
-      setPeerId(id);
-      setPeer(newPeer);
-    });
+ useEffect(() => {
+   const newPeer = new Peer(generateID());
+   newPeer.on('open', id => {
+     setPeerId(id);
+   });
 
-    newPeer.on('connection', c => {
-      setConn(c);
-      setupConnection(c);
-    });
-  }, []);
+   newPeer.on('connection', c => {
+     setConn(c);
+     setupConnection(c);
+   });
+ }, []);
 
-  const setupConnection = (connection) => {
-    setConn(connection);
-    connection.on('open', () => {
-      alert('A client has connected!');
-    });
+ const setupConnection = (connection) => {
+   setConn(connection);
+   connection.on('open', () => {
+     alert('A client has connected!');
+   });
 
-    connection.on('data', data => {
-      handleData(data)
-    });
-  };
+   connection.on('data', data => {
+     handleData(data)
+   });
+ };
 
-  const sendData = () => {
-    if (conn && conn.open) {
-      setIdeas(prevIdeas => [...prevIdeas, message]);
-      conn.send(ideas);
-      setMessage('');
-    }
-  };
+ const sendData = () => {
+   if (conn && conn.open) {
+     setIdeas(prevIdeas => [...prevIdeas, message]);
+     conn.send({ message });
+     setMessage('');
+   }
+ };
 
-  const handleData = (data) => {
-    console.log(data)
-  }
+ const handleData = (data) => {
+   setIdeas(prevIdeas => [...prevIdeas, data.message]);
+ };
 
-  return (
-    <div>
-      <h2>Host Panel</h2>
-      <p>Host ID: {peerId}</p>
-      <input
-        type="text"
-        value={message}
-        onChange={e => setMessage(e.target.value)}
-        placeholder="Type your idea here"
-      />
-      <button onClick={sendData}>Send Idea</button>
-      <ul>
-        {ideas.map((idea, index) => (
-          <li key={index}>{idea}</li>
-        ))}
-      </ul>
-    </div>
-  );
+ return (
+   <div>
+     <h2>Host Panel</h2>
+     <p>Host ID: {peerId}</p>
+     <input
+       type="text"
+       value={message}
+       onChange={e => setMessage(e.target.value)}
+       placeholder="Type your idea here"
+     />
+     <button onClick={sendData}>Send Idea</button>
+     <ul>
+       {ideas.map((idea, index) => (
+         <li key={index}>{idea}</li>
+       ))}
+     </ul>
+   </div>
+ );
 };
 
 export default Host;
