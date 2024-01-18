5c0e1a122a7c09d0d0a7e812bbafb49b6276d77a
Lucas Bateson <lucas.bateson@gmail.com>
2024-01-18 10:03:37 +0100

    Add presentation

---
+++ src/resources/Presentation.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Presentation = () => {
+  return (
+    <div width="100vw" height="80vh">
+      <iframe src='https://github.com/Lucas8448/PostIt/blob/main/Presentation.pdf' width="100vw" height="80vh"/>
+    </div>
+  );
+};
+
+export default Presentation;
