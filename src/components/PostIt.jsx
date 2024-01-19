6275936e32f89dc4cdba8400045aee8c239d0aec
Lucas Bateson <lucas.bateson@gmail.com>
2024-01-19 11:17:25 +0100

    Update post it styling

--- src/components/PostIt.jsx
+++ src/components/PostIt.jsx
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types';
 
 const PostIt = ({ id, content, submitter, submitterEmail }) => {
   return (
-    <div className="post-it" id={`post-it-${id}`}>
+    <div className="post-it-note" id={`post-it-${id}`}>
       <p className="content">{content}</p>
       {submitter && <p className="submitter">{submitter}</p>}
       {submitterEmail && <p className="submitter-email">{submitterEmail}</p>}
